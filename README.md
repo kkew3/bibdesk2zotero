c625ac8a22519a7ca5a3a348604c1d0a04e89908
kkew3 <kps6326@hotmail.com>
2025-04-28 16:13:05 +0800

    Rewrite setup.py

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
-# bibdesk2zotero
+# Migrate from BibDesk to Zotero 7
 
-This is a little utility for rewriting a BibDesk BibTeX file so that it can be
-read by Zotero with the file references intact. BibDesk and Zotero mostly get
-along except for the way BibFile stores links to documents like PDFs. This
-utility Base64 decodes all Bdesk-File paths, parses the serialized [plist],
-extracts the relative path to the file and adds it back to the bibliographic
-entry as an absolute path.
-
-[plist]: https://en.wikipedia.org/wiki/Property_list
+This repository features a robust migration script from BibDesk to Zotero 7.
 
 ## Install
 
-    pip install bibdesk2zotero 
+Install by `pipx` or `uv tool`:
+
+```bash
+uv tool install 'git+https://github.com/kkew3/bibdesk2zotero.git'
+```
 
 ## Use
 
-    $ bibdesk2zotero citations.bib /path/to/pdf/files/ > new-citations.bib
+Run in shell (e.g. bash):
+
+```bash
+bibdesk2zotero -b /path/to/pdf/files citations.bib > new-citations.bib
+```
+
+Logging goes to stderr.
+
+## References:
+
+- [edsu/bibdesk2zotero](https://github.com/edsu/bibdesk2zotero): for how we should interpret the attachment encoding in BibDesk bib file.
--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open("README.md") as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name = 'bibdesk2zotero',
-    version = '0.0.3',
-    author = 'Ed Summers',
-    author_email = 'ehs@pobox.com',
-    url = 'https://github.com/edsu/bibdesk2zotero',
-    py_modules = ['bibdesk2zotero',],
-    description = 'convert BibDesk BibTeX files for import into Zotero',
+    name='bibdesk2zotero',
+    version='0.1.0',
+    author='Kaiwen Wu',
+    url='https://github.com/kkew3/bibdesk2zotero',
+    description='Migration script from BibDesk to Zotero 7',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    install_requires = ['pybtex'],
-    entry_points = {'console_scripts': ['bibdesk2zotero = bibdesk2zotero:main']},
+    python_requires='>=3.9',
+    install_requires = [
+        'bibtexparser<2,>=1.4',
+        'click<9,>=8.1',
+        'loguru>=0.7',
+    ],
+    extras_require={
+        'dev': [
+            'pytest>=8.3',
+        ],
+    },
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    entry_points = {
+        'console_scripts': [
+            'bibdesk2zotero = bibdesk2zotero.bibdesk2zotero:main',
+        ],
+    },
 )
